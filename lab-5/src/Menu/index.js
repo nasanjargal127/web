b1a39b5f2634a08dabebd11aeb4532e6b97ee614
Nasanjargal127 <s.nasaa0127@gmail.com>
2021-04-21 14:55:57 +0800

    Mobile haragdah menu nemew

--- lab-5/src/Menu/index.js
+++ lab-5/src/Menu/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import MenuItem from './MenuItem'
 import Title from '../Title'
 import css from './style.module.css'
+import HamburgerMenu from '../HamburgerMenu'
 // Menu hesgiig gargah component
 const Menu = (props) => {
     return(
         <div className={css.title}>
             <Title text="MY REACT WEBSITE"/>
             <ul className={css.Menu}>
+                <HamburgerMenu />
             <MenuItem>OVERVIEW</MenuItem>
             <MenuItem>FEATURES</MenuItem>
             <MenuItem>DETAILS</MenuItem>
